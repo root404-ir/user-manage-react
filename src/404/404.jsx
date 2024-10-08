00d14eab71dc676e24c8d0557630cc341cdc780c
root404 <mohammad>
2024-10-08 08:48:11 +0330

    add 404 page

---
+++ src/404/404.jsx
@@ -0,0 +1,13 @@
+import './404.css'
+const ErrPage = ()=>{
+    return(
+        <div className='err'>
+            <h3>
+                <span className='title-err title-err1'>error</span>
+                <span className='title-err title-err2'>404</span>
+            </h3>
+            <span className='subTitle'> همچین صفحه ای وجود نداره عزیزم ):</span>
+        </div>
+    )
+}
+export default ErrPage
